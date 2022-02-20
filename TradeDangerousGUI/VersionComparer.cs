8cda685aa3500cc146f7e0af2f526f8d9500e9bd
Andrew Parish <andrew@parishfamily.co.uk>
2022-02-20 21:08:48 +0000

    Fix for Issue-#21
    Added a new version comparer to sort the list of versions from the pypi site correctly. Now the program correctly understands that 10.9.8 is actually earlier than 10.10.1!

---
+++ TradeDangerousGUI/VersionComparer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TDHelper
+{
+    class VersionComparer : Comparer<string>
+    {
+        public override int Compare(string x, string y)
+        {
+            Version xVersion = new Version(x);
+            Version yVersion = new Version(y);
+
+            return xVersion.CompareTo(yVersion);
+        }
+    }
+}
